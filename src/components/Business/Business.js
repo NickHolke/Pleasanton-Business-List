3fa6026175604277f437f3e4f6ce6f1696c24ad1
Nick Holke <nickholke@gmail.com>
2020-04-10 14:56:26 -0700

    Added new font and color scheme

--- src/components/Business/Business.js
+++ src/components/Business/Business.js
@@ -1,30 +1,30 @@
 import React from 'react';
-import { wrapper, title, icon, row, plus} from './Business.module.css';
+import { wrapper, title, icon, row, plus,} from './Business.module.css';
 import linkIcon from './linkIcon.svg';
 import addressIcon from './address.svg';
 import phoneIcon from './phone.svg';
 import plusIcon from './more.svg';
 
 const Business = ({info}) => (
-  <div className={wrapper}>
-    <h1 className={title}>{info.Name}</h1>
-    <h2>{info.Status}</h2>
-    <div className={row}>
-      <div>
-        <img className={icon} src={addressIcon} alt="address"/>
+    <div className={wrapper}>
+      <h1 className={title}>{info.Name}</h1>
+      <h2>{info.Status}</h2>
+      <div className={row}>
+        <div>
+          <img className={icon} src={addressIcon} alt="address"/>
+        </div>
+        <p>{info.Address}</p>
       </div>
-      <p>{info.Address}</p>
+      <div className={row}>
+        <img className={icon} src={phoneIcon} alt="phone"/>
+        <p>{info.Phone}</p>
+      </div>
+      <div className={row}>
+        <img className={icon} src={linkIcon} alt="link"/>
+        <a href={info.Website}>website</a>
+      </div>
+      <img className={plus} src={plusIcon} alt="more-info"/>
     </div>
-    <div className={row}>
-      <img className={icon} src={phoneIcon} alt="phone"/>
-      <p>{info.Phone}</p>
-    </div>
-    <div className={row}>
-      <img className={icon} src={linkIcon} alt="link"/>
-      <a href={info.Website}>website</a>
-    </div>
-    <img className={plus} src={plusIcon} alt="more-info"/>
-  </div>
 )
 
 export default Business;
